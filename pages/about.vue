2b500c6b0335f27336e5f4eac13e03ed71ef2bc7
William Gonçalves <euwilliamgoncalves@gmail.com>
2020-12-11 18:18:47 -0300

    Página "sobre"

---
+++ pages/about.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="content--container" class="about-content">
+    <section>
+      <div class="section--text">
+        <h3>{{ dynamicText }}</h3>
+        <p>William Gonçalves, carioca, natural da Zona Norte (Alô, Andaraí!), mas andarilho da Oeste e da Baixada, há mais de 15 anos. Flamenguista roxo, na alegria e na sofrência. Pai de um príncipe de 3 anos que me enche de orgulho e me mostra como A Vida é Bela e digna de um Oscar, como Roberto Benigni.</p>
+        <p>
+          Canhoto nascido na Primavera de 1988, que desde cedo fez jus a fama dos 'left-handed', com ótima noção espacial, criatividade e tendência as atividades artísticas. Os astros, na hora registrada em certidão, garantiram intensidade, determinação, inconformismo, espontaneidade, livre-pensamento, grande
+          imaginação e sensibilidade. Mas esse papo a gente deixa pra quem curte.
+        </p>
+        <p>
+          Designer "autodidata" (entre aspas, porque desde o advento da internet, ninguém aprende nada sozinho...hahaha) há tanto tempo que já nem sei o quanto, mas oficialmente atuante desde 2016, quando comecei a levar o tal talento mais a sério. Trabalhei com muitos clientes em vários projetos bacanas, incluindo o
+          primeiro mapa do transporte metropolitano do Rio para o app da Moovit, trampo que rendeu uma matéria no Jornal O Dia, na época.
+        </p>
+        <p>Desenvolvedor em Formação, apaixonado por Front-End, com estudos focados no desenvolvimento web, desde HTML, CSS e JavaScript, até React e Vue.js, além de conceitos de Design, UI, UX e usabilidade.</p>
+        <p>Sabe o cara que você busca pra pensar o não óbvio em seus projetos e chegar a um resultado de encher os olhos? Esse cara sou eu! (São tantas emoções, bicho!)</p>
+      </div>
+      <img src="~/assets/images/pages/about/profile.jpg" alt="" />
+    </section>
+    <section>
+      <div class="section--text">
+        <h3>De onde o Mago surgiu?</h3>
+        <p>
+          O Mago nasceu de uma brincadeira durante o Mega Hack 3.0 da Shawee, do qual participei, no desafio da Árvore Educação. Meu parceiro de equipe e amigo nessa jornada Dev, <a href="https://www.linkedin.com/in/thiagopederzollimdasilva/" target="_blank" rel="noreferrer noopener">Thiago Pederzolli</a>, me deu o
+          apelido 'Mago do CSS', em função das minhas habilidades com estilização.
+        </p>
+        <p>
+          Aproveitando a alcunha, comecei a compartilhar conteúdo no Instagram, no LinkedIn e no YouTube, através do meu canal, falando sobre propriedades e recursos do CSS. Mas ao perceber que poderia explorar outros temas, reduzi o nome a 'Mago', para que outros assuntos possam ser abordados em publicações futuras.
+        </p>
+        <p>A ideia é usar essa marca pessoal para replicar conhecimento e compartilhar o aprendizado com outros desevolvedores que, assim como eu, têm trilhado essa estrada do desenvolvimento Web.</p>
+      </div>
+      <img src="~/assets/images/pages/about/mage-in-red.png" alt="" />
+    </section>
+    <section>
+      <div class="section--text">
+        <h3>Sobre esse portif*da</h3>
+        <p>
+          Essa página linda de bonita foi feita com <a href="https://nuxtjs.org/" target="_blank" rel="noreferrer noopener">Nuxt.js</a>, um framework baseado em <a href="https://vuejs.org/" target="_blank" rel="noreferrer noopener">Vue.js</a>, que permite desenvolver aplicações Front-End com o pé nas costas (até
+          porque, você coda com as mãos).
+        </p>
+        <p>O design e os estilos são de minha autoria (chefe é chefe, pai) e foram feitos com <a href="https://sass-lang.com/" target="_blank" rel="noreferrer noopener">Sass</a> em sintaxe SCSS.</p>
+        <p>Todas as artes e animações também são autorais, com exceção do vídeo na página inicial, que foi feito no app <a href="https://zepeto.me/" target="_blank" rel="noreferrer noopener">Zepeto</a>.</p>
+        <h5>Menções honrosas</h5>
+        <p>Por tudo que cresci e aprendi nessa jornada, meus sinceros agradecimentos (e recomendações: segue lá, véi!) a essas feras abaixo:</p>
+        <p>
+          <a href="https://www.linkedin.com/in/diego-schell-fernandes/" target="_blank" rel="noreferrer noopener">Diego Fernandes</a>, <a href="https://www.linkedin.com/in/maykbrito/" target="_blank" rel="noreferrer noopener">Mayk Brito</a> e toda equipe da
+          <a href="https://rocketseat.com.br/" target="_blank" rel="noreferrer noopener">Rocketseat</a>, pelas imersões e eventos através dos quais pude crescer um monte.
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/iglageneroso/" target="_blank" rel="noreferrer noopener">Iglá Generoso</a> e todo o time da <a href="https://digitalinnovation.one/" target="_blank" rel="noreferrer noopener">Digital Innovation One</a> por me tirarem da inércia acadêmica e fazerem eu me apaixonar pela
+          programação, mais uma vez.
+        </p>
+        <p><a href="https://www.linkedin.com/in/rafaelmaiach/" target="_blank" rel="noreferrer noopener">Rafael Maia</a> por me apresentar o mundo Vue.js e toda a equipe da <a href="https://vueschool.io/" target="_blank" rel="noreferrer noopener">VueSchool</a> pelo excelente conteúdo e aprendizado na plataforma.</p>
+        <p>
+          E pelo apoio e experiências compartilhadas nessa jornada, um abraço apertadão a essas pessoas queridas que me inspiram demais:
+          <a href="https://www.linkedin.com/in/beatriiz-oliveiraa/" target="_blank" rel="noreferrer noopener">Beatriz Oliveira</a>, <a href="https://www.linkedin.com/in/bruna-g-4a65b9127/" target="_blank" rel="noreferrer noopener">Bruna Gallo</a>,
+          <a href="https://www.linkedin.com/in/nolascobruna/" target="_blank" rel="noreferrer noopener">Bruna Nolasco</a>, <a href="https://www.instagram.com/codecami/" target="_blank" rel="noreferrer noopener">Camila Paixão</a>,
+          <a href="https://www.linkedin.com/in/nayra-cruz/" target="_blank" rel="noreferrer noopener">Nayra Cruz</a>, <a href="https://www.instagram.com/vanribeiro.dev/" target="_blank" rel="noreferrer noopener">Van Ribeiro</a>,
+          <a href="https://www.linkedin.com/in/danielmeiato/" target="_blank" rel="noreferrer noopener">Daniel Meiato</a>, <a href="https://www.linkedin.com/in/eduardo-freitas-48b7bb19b/" target="_blank" rel="noreferrer noopener">Eduardo Freitas</a>,
+          <a href="https://www.linkedin.com/in/igor-rebolla/" target="_blank" rel="noreferrer noopener">Igor Rebolla</a>, <a href="https://www.linkedin.com/in/bernardojachegou/" target="_blank" rel="noreferrer noopener">Michel Bernardo</a>,
+          <a href="https://www.linkedin.com/in/thiagopederzollimdasilva/" target="_blank" rel="noreferrer noopener">Thiago Pederzolli</a> e <a href="https://www.linkedin.com/in/tiago-barcelos-98646a9a/" target="_blank" rel="noreferrer noopener">Tiago Barcelos</a>.
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      dynamicText: "",
+      aboutTitle: "O homem por trás da mágica",
+      characterIndex: 0,
+    };
+  },
+  methods: {
+    writedynamicText() {
+      if (this.dynamicText.length < this.aboutTitle.length) {
+        this.dynamicText += this.aboutTitle.charAt(this.characterIndex);
+        this.characterIndex++;
+        setTimeout(this.writedynamicText, 50);
+      }
+    },
+  },
+  created() {
+    this.writedynamicText();
+  },
+});
+</script>
+
+<style lang="scss">
+.about-content {
+  @include content--base--layout;
+  justify-content: flex-start;
+
+  section {
+    @include section--left--base;
+
+    img {
+      @include right--full--image;
+      cursor: $hover--emoji--love;
+    }
+
+    &:nth-child(2) {
+      @include section--right--base;
+
+      img {
+        @include left--full--image;
+        cursor: $hover--emoji--mage;
+      }
+    }
+  }
+}
+</style>
+
+<style></style>
