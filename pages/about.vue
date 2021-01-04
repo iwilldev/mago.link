64a73a27ab023dccba36dcc185aa6572a94a3379
William Gonçalves <euwilliamgoncalves@gmail.com>
2021-01-04 10:43:08 -0300

    Página "Sobre"

--- pages/about.vue
+++ pages/about.vue
@@ -1,64 +1,164 @@
 <template>
   <div id="content--container" class="about-content">
-    <section>
-      <div class="section--text">
-        <h3>{{ dynamicText }}</h3>
-        <p>William Gonçalves, carioca, natural da Zona Norte (Alô, Andaraí!), mas andarilho da Oeste e da Baixada, há mais de 15 anos. Flamenguista roxo, na alegria e na sofrência. Pai de um príncipe de 3 anos que me enche de orgulho e me mostra como A Vida é Bela e digna de um Oscar, como Roberto Benigni.</p>
+    <section class="section--left--base">
+      <img
+        class="section--image"
+        src="~/assets/images/pages/about/profile.jpg"
+        alt="Minha foto, tirada pelo lado esquerdo do rosto. Estou de óculos e esboçando um sorriso de lábios cerrados."
+        :class="isMenuVisible ? 'invisible--content' : ''"
+      />
+      <div class="text--container" :class="isMenuVisible ? 'invisible--content' : ''">
+        <h3><base-dynamic-text text-to-be-written="O homem por trás da mágica" /></h3>
         <p>
-          Canhoto nascido na Primavera de 1988, que desde cedo fez jus a fama dos 'left-handed', com ótima noção espacial, criatividade e tendência as atividades artísticas. Os astros, na hora registrada em certidão, garantiram intensidade, determinação, inconformismo, espontaneidade, livre-pensamento, grande
-          imaginação e sensibilidade. Mas esse papo a gente deixa pra quem curte.
+          William Gonçalves, carioca, natural da Zona Norte (Alô, Andaraí!), mas andarilho
+          da Oeste e da Baixada, há mais de 15 anos. Flamenguista roxo, na alegria e na
+          sofrência. Pai de um príncipe de 3 anos que me enche de orgulho e me mostra como
+          A Vida é Bela e digna de um Oscar, como Roberto Benigni.
         </p>
         <p>
-          Designer "autodidata" (entre aspas, porque desde o advento da internet, ninguém aprende nada sozinho...hahaha) há tanto tempo que já nem sei o quanto, mas oficialmente atuante desde 2016, quando comecei a levar o tal talento mais a sério. Trabalhei com muitos clientes em vários projetos bacanas, incluindo o
-          primeiro mapa do transporte metropolitano do Rio para o app da Moovit, trampo que rendeu uma matéria no Jornal O Dia, na época.
+          Canhoto nascido na Primavera de 1988, que desde cedo fez jus a fama dos
+          'left-handed', com ótima noção espacial, criatividade e tendência as atividades
+          artísticas. Os astros, na hora registrada em certidão, garantiram intensidade,
+          determinação, inconformismo, espontaneidade, livre-pensamento, grande imaginação
+          e sensibilidade. Mas esse papo a gente deixa pra quem curte.
         </p>
-        <p>Desenvolvedor em Formação, apaixonado por Front-End, com estudos focados no desenvolvimento web, desde HTML, CSS e JavaScript, até React e Vue.js, além de conceitos de Design, UI, UX e usabilidade.</p>
-        <p>Sabe o cara que você busca pra pensar o não óbvio em seus projetos e chegar a um resultado de encher os olhos? Esse cara sou eu! (São tantas emoções, bicho!)</p>
+        <p>
+          Designer "autodidata" (entre aspas, porque desde o advento da internet, ninguém
+          aprende nada sozinho...hahaha) há tanto tempo que já nem sei o quanto, mas
+          oficialmente atuante desde 2016, quando comecei a levar o tal talento mais a
+          sério. Trabalhei com muitos clientes em vários projetos bacanas, incluindo o
+          primeiro mapa do transporte metropolitano do Rio para o app da Moovit, trampo
+          que rendeu uma matéria no Jornal O Dia, na época.
+        </p>
+        <p>
+          Desenvolvedor em Formação, apaixonado por Front-End, com estudos focados no
+          desenvolvimento web, desde HTML, CSS e JavaScript, até React e Vue.js, além de
+          conceitos de Design, UI, UX e usabilidade.
+        </p>
+        <p>
+          Voltei para o universo da programação em meio a pandemia, depois de 10 anos de
+          uma graduação (Sistemas de Informação) trancada no último período. Atuante em
+          outro segmento (Energia Segura), entendia que não fazia sentido finalizar o
+          curso e decidi continuar focado no meu trabalho. Uma década depois, mais maduro
+          e com um propósito redirecionado, ressignifiquei minha paixão pelos códigos e
+          descobri nisso a minha real vocação.
+        </p>
+        <p>
+          Sabe o cara que você busca pra pensar o não óbvio em seus projetos e chegar a um
+          resultado de encher os olhos? Esse cara sou eu! (São tantas emoções, bicho!)
+        </p>
       </div>
-      <img src="~/assets/images/pages/about/profile.jpg" alt="" />
     </section>
-    <section>
-      <div class="section--text">
+    <section class="section--right--base">
+      <img
+        class="section--image"
+        src="~/assets/images/pages/about/mage-in-red.png"
+        alt="Ícone do logo do Mago em cor branca com fundo vermelho."
+        :class="isMenuVisible ? 'invisible--content' : ''"
+      />
+      <div class="text--container" :class="isMenuVisible ? 'invisible--content' : ''">
         <h3>De onde o Mago surgiu?</h3>
         <p>
-          O Mago nasceu de uma brincadeira durante o Mega Hack 3.0 da Shawee, do qual participei, no desafio da Árvore Educação. Meu parceiro de equipe e amigo nessa jornada Dev, <a href="https://www.linkedin.com/in/thiagopederzollimdasilva/" target="_blank" rel="noreferrer noopener">Thiago Pederzolli</a>, me deu o
-          apelido 'Mago do CSS', em função das minhas habilidades com estilização.
+          O Mago nasceu de uma brincadeira durante o Mega Hack 3.0 da Shawee, do qual
+          participei, no desafio da Árvore Educação. Meu parceiro de equipe e amigo nessa
+          jornada Dev,
+          <a
+            href="https://www.linkedin.com/in/thiagopederzollimdasilva/"
+            target="_blank"
+            rel="noreferrer noopener"
+            >Thiago Pederzolli</a
+          >, me deu o apelido 'Mago do CSS', em função das minhas habilidades com
+          estilização.
         </p>
         <p>
-          Aproveitando a alcunha, comecei a compartilhar conteúdo no Instagram, no LinkedIn e no YouTube, através do meu canal, falando sobre propriedades e recursos do CSS. Mas ao perceber que poderia explorar outros temas, reduzi o nome a 'Mago', para que outros assuntos possam ser abordados em publicações futuras.
+          Aproveitando a alcunha, comecei a compartilhar conteúdo no Instagram, no
+          LinkedIn e no YouTube, através do meu canal, falando sobre propriedades e
+          recursos do CSS. Mas ao perceber que poderia explorar outros temas, reduzi o
+          nome a 'Mago', para que outros assuntos possam ser abordados em publicações
+          futuras.
         </p>
-        <p>A ideia é usar essa marca pessoal para replicar conhecimento e compartilhar o aprendizado com outros desevolvedores que, assim como eu, têm trilhado essa estrada do desenvolvimento Web.</p>
+        <p>
+          A ideia é usar essa marca pessoal para replicar conhecimento e compartilhar o
+          aprendizado com outros desevolvedores que, assim como eu, têm trilhado essa
+          estrada do desenvolvimento Web.
+        </p>
       </div>
-      <img src="~/assets/images/pages/about/mage-in-red.png" alt="" />
     </section>
-    <section>
-      <div class="section--text">
+    <section class="section--left--base">
+      <img
+        class="section--image"
+        src="~/assets/images/pages/about/stacks-used.png"
+        alt="Ícone de fechamento de tag HTML, com os logos do Nuxt.js, do Vue.js e do Sass, abaixo."
+        :class="isMenuVisible ? 'invisible--content' : ''"
+      />
+      <div class="text--container" :class="isMenuVisible ? 'invisible--content' : ''">
         <h3>Sobre esse portif*da</h3>
         <p>
-          Essa página linda de bonita foi feita com <a href="https://nuxtjs.org/" target="_blank" rel="noreferrer noopener">Nuxt.js</a>, um framework baseado em <a href="https://vuejs.org/" target="_blank" rel="noreferrer noopener">Vue.js</a>, que permite desenvolver aplicações Front-End com o pé nas costas (até
-          porque, você coda com as mãos).
-        </p>
-        <p>O design e os estilos são de minha autoria (chefe é chefe, pai) e foram feitos com <a href="https://sass-lang.com/" target="_blank" rel="noreferrer noopener">Sass</a> em sintaxe SCSS.</p>
-        <p>Todas as artes e animações também são autorais, com exceção do vídeo na página inicial, que foi feito no app <a href="https://zepeto.me/" target="_blank" rel="noreferrer noopener">Zepeto</a>.</p>
-        <h5>Menções honrosas</h5>
-        <p>Por tudo que cresci e aprendi nessa jornada, meus sinceros agradecimentos (e recomendações: segue lá, véi!) a essas feras abaixo:</p>
-        <p>
-          <a href="https://www.linkedin.com/in/diego-schell-fernandes/" target="_blank" rel="noreferrer noopener">Diego Fernandes</a>, <a href="https://www.linkedin.com/in/maykbrito/" target="_blank" rel="noreferrer noopener">Mayk Brito</a> e toda equipe da
-          <a href="https://rocketseat.com.br/" target="_blank" rel="noreferrer noopener">Rocketseat</a>, pelas imersões e eventos através dos quais pude crescer um monte.
+          Essa página linda de bonita foi feita com
+          <a href="https://nuxtjs.org/" target="_blank" rel="noreferrer noopener"
+            >Nuxt.js</a
+          >, um framework baseado em
+          <a href="https://vuejs.org/" target="_blank" rel="noreferrer noopener">Vue.js</a
+          >, que permite desenvolver aplicações Front-End com o pé nas costas (até porque,
+          você coda com as mãos).
         </p>
         <p>
-          <a href="https://www.linkedin.com/in/iglageneroso/" target="_blank" rel="noreferrer noopener">Iglá Generoso</a> e todo o time da <a href="https://digitalinnovation.one/" target="_blank" rel="noreferrer noopener">Digital Innovation One</a> por me tirarem da inércia acadêmica e fazerem eu me apaixonar pela
-          programação, mais uma vez.
+          O design e os estilos são de minha autoria (chefe é chefe, pai) e foram feitos
+          com
+          <a href="https://sass-lang.com/" target="_blank" rel="noreferrer noopener"
+            >Sass</a
+          >
+          em sintaxe SCSS.
         </p>
-        <p><a href="https://www.linkedin.com/in/rafaelmaiach/" target="_blank" rel="noreferrer noopener">Rafael Maia</a> por me apresentar o mundo Vue.js e toda a equipe da <a href="https://vueschool.io/" target="_blank" rel="noreferrer noopener">VueSchool</a> pelo excelente conteúdo e aprendizado na plataforma.</p>
         <p>
-          E pelo apoio e experiências compartilhadas nessa jornada, um abraço apertadão a essas pessoas queridas que me inspiram demais:
-          <a href="https://www.linkedin.com/in/beatriiz-oliveiraa/" target="_blank" rel="noreferrer noopener">Beatriz Oliveira</a>, <a href="https://www.linkedin.com/in/bruna-g-4a65b9127/" target="_blank" rel="noreferrer noopener">Bruna Gallo</a>,
-          <a href="https://www.linkedin.com/in/nolascobruna/" target="_blank" rel="noreferrer noopener">Bruna Nolasco</a>, <a href="https://www.instagram.com/codecami/" target="_blank" rel="noreferrer noopener">Camila Paixão</a>,
-          <a href="https://www.linkedin.com/in/nayra-cruz/" target="_blank" rel="noreferrer noopener">Nayra Cruz</a>, <a href="https://www.instagram.com/vanribeiro.dev/" target="_blank" rel="noreferrer noopener">Van Ribeiro</a>,
-          <a href="https://www.linkedin.com/in/danielmeiato/" target="_blank" rel="noreferrer noopener">Daniel Meiato</a>, <a href="https://www.linkedin.com/in/eduardo-freitas-48b7bb19b/" target="_blank" rel="noreferrer noopener">Eduardo Freitas</a>,
-          <a href="https://www.linkedin.com/in/igor-rebolla/" target="_blank" rel="noreferrer noopener">Igor Rebolla</a>, <a href="https://www.linkedin.com/in/bernardojachegou/" target="_blank" rel="noreferrer noopener">Michel Bernardo</a>,
-          <a href="https://www.linkedin.com/in/thiagopederzollimdasilva/" target="_blank" rel="noreferrer noopener">Thiago Pederzolli</a> e <a href="https://www.linkedin.com/in/tiago-barcelos-98646a9a/" target="_blank" rel="noreferrer noopener">Tiago Barcelos</a>.
+          Todas as artes e animações também são autorais, com exceção do vídeo na página
+          inicial, que foi feito no app
+          <a href="https://zepeto.me/" target="_blank" rel="noreferrer noopener">Zepeto</a
+          >, e dos ícones das stacks e das ferramentas, no portfólio, que foram pegos no
+          <a href="https://devicon.dev/" target="_blank" rel="noreferrer noopener"
+            >DEVICON</a
+          >
+          ou na
+          <a href="https://pt.wikipedia.org/" target="_blank" rel="noreferrer noopener"
+            >Wikipédia</a
+          >
+          e dos ícones das redes sociais, que foram retirados de seus respectivos sites.
+        </p>
+        <p>E sim: ele roda na minha máquina.</p>
+        <h3>Notas de compatibilidade:</h3>
+        <p>
+          Por utilizar recursos modernos de CSS, essa página terá o comportamento esperado
+          se for usada nos seguintes navegadores, nas versões citadas:
+        </p>
+        <p>
+          Chrome 84+ (PC/Móvel) <br />
+          Firefox 79+ (PC) <br />
+          Edge 84+ (PC) <br />
+          Opera 70+ (PC) <br />
+        </p>
+        <p>
+          O Safari para desktop, mesmo nas versões mais recentes, pode apresentar
+          problemas de espaçamento, por não suportar a propriedade 'gap' em layouts flex.
+          O Internet Explorer não exibirá a página corretamente, por não suportar os
+          recursos do CSS Flexbox.
+        </p>
+        <p>
+          Nos dispositivos móveis, Firefox 63+ cairá num fallback e poderá apresentar
+          textos maiores que o esperado, dependendo do tamanho de tela, por não suportar a
+          função 'clamp()', utilizada para ajustar o 'font-size', conforme mudança de
+          tela.
+        </p>
+        <p>
+          O Safari Móvel apresentará problemas de espaçamento, assim como a versão
+          desktop. O mesmo acontece com Samsung Internet e Opera Móvel. Esse último, além
+          da incompatibilidade do 'gap' em layout flex, também apresenta incompatibilidade
+          com a função 'clamp()', assim como o Firefox 63+.
+        </p>
+        <p>
+          Navegadores alternativos com engines baseadas nas das versões suportadas podem
+          funcionar devidamente, mas não foram feitos testes abertos. Versões anteriores
+          as citadas podem não ter compatibilidade (total ou parcial), com os recursos do
+          site.
         </p>
       </div>
     </section>
@@ -67,53 +167,64 @@
 
 <script>
 import Vue from "vue";
+import BaseDynamicText from "~/components/BaseDynamicText.vue";
 
 export default Vue.extend({
-  data() {
-    return {
-      dynamicText: "",
-      aboutTitle: "O homem por trás da mágica",
-      characterIndex: 0,
-    };
+  components: { BaseDynamicText },
+  head: {
+    title: `Sobre mim | William 'Mago' Gonçalves`,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "O homem por trás da mágica. Conheça minha história, descubra como o 'Mago' surgiu e saiba mais sobre essa página.",
+      },
+      { property: "og:title", content: "Sobre mim | William 'Mago' Gonçalves" },
+      { property: "og:site_name", content: "mago.link" },
+      {
+        property: "og:description",
+        content:
+          "O homem por trás da mágica. Conheça minha história, descubra como o 'Mago' surgiu e saiba mais sobre essa página.",
+      },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://mago.link/" },
+      { property: "og:image", content: "https://mago.link/og-background.png" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
-  methods: {
-    writedynamicText() {
-      if (this.dynamicText.length < this.aboutTitle.length) {
-        this.dynamicText += this.aboutTitle.charAt(this.characterIndex);
-        this.characterIndex++;
-        setTimeout(this.writedynamicText, 50);
-      }
+  computed: {
+    isMenuVisible() {
+      return this.$store.state.menu.isMenuVisible;
     },
-  },
-  created() {
-    this.writedynamicText();
   },
 });
 </script>
 
 <style lang="scss">
-.about-content {
-  @include content--base--layout;
-  justify-content: flex-start;
-
-  section {
-    @include section--left--base;
-
-    img {
-      @include right--full--image;
+section {
+  &:nth-child(1) {
+    .section--image {
       cursor: $hover--emoji--love;
     }
+  }
 
-    &:nth-child(2) {
-      @include section--right--base;
-
-      img {
-        @include left--full--image;
-        cursor: $hover--emoji--mage;
-      }
+  &:nth-child(3) {
+    .section--image {
+      cursor: $hover--emoji--code;
     }
+  }
+
+  &:nth-child(2) {
+    .section--image {
+      cursor: $hover--emoji--mage;
+    }
+  }
+
+  .section--image {
+    align-self: center;
   }
 }
 </style>
-
-<style></style>
